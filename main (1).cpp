89159eae292fc6de6f4f80a738036f9dceb1031e
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-04-29 10:12:32 +0800

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void fibo(int a, int b, int p){
+    int cnt = 0, t;
+    while(cnt != p - 2){
+        t = a + b;
+        a = b;
+        cout << t << " ";
+        ++cnt;
+        b = t;
+    }
+}
+int main(){
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+    printf("%d %d " , n1, n2);
+    fibo(n1, n2, n3);
+    return 0;
+}
